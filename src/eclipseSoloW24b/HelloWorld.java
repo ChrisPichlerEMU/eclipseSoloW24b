e947dbf1fb7935d7ae646a9de35d0edaaac88a13
Chris Pichler <cpiche1@emich.edu>
2024-02-12 10:50:21 -0500

    Another println statement was added!

--- src/eclipseSoloW24b/HelloWorld.java
+++ src/eclipseSoloW24b/HelloWorld.java
@@ -3,7 +3,8 @@ package eclipseSoloW24b;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Howdy github Project");
+		System.out.println("Hello world");
+		System.out.println("Another println statement");
 	}
 
 }
